ae4e6c718d8940632eb15b3c2eae966e34395f1b
David Jimenez <davidrumsjb@icloud.com>
2016-05-05 15:57:36 +0200

    Ilias Contribution

--- src/main/java/com/github/rinde/rinsim/examples/experiment/AgvAgent.java
+++ src/main/java/com/github/rinde/rinsim/examples/experiment/AgvAgent.java
@@ -3,11 +3,13 @@ package com.github.rinde.rinsim.examples.experiment;
 import com.github.rinde.rinsim.core.model.pdp.Vehicle;
 import com.github.rinde.rinsim.core.model.pdp.VehicleDTO;
 import com.github.rinde.rinsim.core.model.time.TimeLapse;
+import com.github.rinde.rinsim.geom.Point;
 
 public class AgvAgent extends Vehicle{
 
 	public AgvAgent(VehicleDTO dto){
 		super(dto);
+		//super(dto).builder().startPosition(initPosi);
 	}
 
 	@Override
